e0a664dfc94eb9bf1b3138ee4f8de25c4005141f
Anik Das <anikdas0811@gmail.com>
2020-11-01 18:49:53 +0530

    - Init Repo

---
+++ components/Profile.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="">
+    <img class="rounded-full h-48" :src="userdetails.avatar_url" alt="" />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      userdetails: {
+        login: 'sadn1ck',
+        id: 16396161,
+        node_id: 'MDQ6VXNlcjE2Mzk2MTYx',
+        avatar_url: 'https://avatars2.githubusercontent.com/u/16396161?v=4',
+        gravatar_id: '',
+        url: 'https://api.github.com/users/sadn1ck',
+        html_url: 'https://github.com/sadn1ck',
+        followers_url: 'https://api.github.com/users/sadn1ck/followers',
+        following_url:
+          'https://api.github.com/users/sadn1ck/following{/other_user}',
+        gists_url: 'https://api.github.com/users/sadn1ck/gists{/gist_id}',
+        starred_url:
+          'https://api.github.com/users/sadn1ck/starred{/owner}{/repo}',
+        subscriptions_url: 'https://api.github.com/users/sadn1ck/subscriptions',
+        organizations_url: 'https://api.github.com/users/sadn1ck/orgs',
+        repos_url: 'https://api.github.com/users/sadn1ck/repos',
+        events_url: 'https://api.github.com/users/sadn1ck/events{/privacy}',
+        received_events_url:
+          'https://api.github.com/users/sadn1ck/received_events',
+        type: 'User',
+        site_admin: false,
+        name: 'Anik Das',
+        company: null,
+        blog: 'https://www.linkedin.com/in/sadn1ck/',
+        location: 'IIEST, Shibpur',
+        email: null,
+        hireable: true,
+        bio: 'Frontend | DevOps | Security @0xw3bs3c ',
+        twitter_username: null,
+        public_repos: 35,
+        public_gists: 3,
+        followers: 40,
+        following: 62,
+        created_at: '2015-12-22T04:54:39Z',
+        updated_at: '2020-10-29T17:04:04Z',
+      },
+    }
+  },
+}
+</script>
+
+<style>
+.force-center {
+  display: grid;
+  place-items: center;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="px-8 md:px-32 lg:px-64 pt-32 md:py-32 mx-auto">
+    <div class="mb-4">
+      <div class="force-center">
+        <label
+          class="block text-gray-700 text-2xl font-bold mb-2 p-5"
+          for="username"
+        >
+          GitHub Profile Visualizer
+        </label>
+      </div>
+      <div class="force-center p-5">
+        <input
+          v-model="username"
+          type="text"
+          class="relative outline-none py-5 px-5 rounded-xl w-full bg-white shadow focus:shadow-2xl text-base text-gray-700 placeholder-gray-400 focus:outline-none transition duration-200"
+          placeholder="Eg., torvalds"
+        />
+      </div>
+      <div class="force-center p-5">
+        <button
+          class="bg-gray-200 hover:bg-green-400 px-10 py-3 rounded transition duration-200 focus:outline-none shadow hover:shadow-2xl"
+          @click="submit"
+        >
+          Submit
+        </button>
+      </div>
+
+      <div v-if="invaliduser" class="force-center">
+        <label
+          class="block text-1xl font-bold mb-2 p-5 text-red-600"
+          for="username"
+        >
+          No such username!
+        </label>
+      </div>
+    </div>
+    <div v-if="receiveddata">
+      <Profile />
+    </div>
+  </div>
+</template>
+
+<script>
+import Profile from '~/components/Profile'
+export default {
+  components: {
+    Profile,
+  },
+  data() {
+    return {
+      valid: null,
+      receiveddata: false,
+      invaliduser: false,
+      username: '',
+      userdetails: {},
+    }
+  },
+  methods: {
+    async submit() {
+      // resetting before sending request so as not to pollute incoming data
+      this.valid = null
+      this.receiveddata = false
+      this.invaliduser = false
+      this.userdetails = {}
+      const data = await fetch(`https://api.github.com/users/${this.username}`)
+      data.json().then((response) => {
+        console.log(response)
+        if (!(response.message === 'Not Found')) {
+          this.receiveddata = true
+          this.userdetails = response
+        } else {
+          this.invaliduser = true
+        }
+      })
+    },
+  },
+}
+</script>
+
+<style>
+.force-center {
+  display: grid;
+  place-items: center;
+}
+</style>
